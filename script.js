3193816c64af78b207a879d75664a3639dbd2a03
Katie <katie.mcy@outlook.com>
2023-04-02 18:28:47 -0400

    begin restructuring about section

--- script.js
+++ script.js
@@ -68,37 +68,39 @@ const scrollListening = () => {
         } 
     // END: header page animation
     
+
     // Fun Fact list slide in animation
         const aboutStickyView = document.querySelector('.aboutStickyView');
     
-        const aboutMaxScroll = aboutStickyView.scrollHeight - window.innerHeight;
-        // 1800 is the height of scroll space until about section comes to sight
-            // aboutScrollFraction = 0 means the about section just arrives top of viewport
-            // when aboutScrollFraction starts to > 1, the about section starts to leave viewport
-        const aboutScrollFraction = (scrollPosition - 1800) / aboutMaxScroll;
+        // const aboutMaxScroll = aboutStickyView.scrollHeight - window.innerHeight;
+        // // 1800 is the height of scroll space until about section comes to sight
+        //     // aboutScrollFraction = 0 means the about section just arrives top of viewport
+        //     // when aboutScrollFraction starts to > 1, the about section starts to leave viewport
+        // const aboutScrollFraction = (scrollPosition - 1800) / aboutMaxScroll;
     
-        const aboutMeLi = document.querySelector('.aboutMeUl').children;
-        const aboutMeLiArray = Array.from(aboutMeLi);
+        // const aboutMeLi = document.querySelector('.aboutMeUl').children;
+        // const aboutMeLiArray = Array.from(aboutMeLi);
     
-        const kickInPoint = [
-            0.10,
-            0.18,
-            0.26
-        ]
+        // const kickInPoint = [
+        //     0.10,
+        //     0.18,
+        //     0.26
+        // ]
     
-        const slideInAnimation = (kickInPoint, li) => {
-            if (aboutScrollFraction > kickInPoint) {
-                li.style.transform = "translateX(0)"
-            } else {
-                li.style.transform = "translateX(-120%)"
-            }
-        }
+        // const slideInAnimation = (kickInPoint, li) => {
+        //     if (aboutScrollFraction > kickInPoint) {
+        //         li.style.transform = "translateX(0)"
+        //     } else {
+        //         li.style.transform = "translateX(-120%)"
+        //     }
+        // }
     
-        for (let i = 0; i < kickInPoint.length; i++) {
-            slideInAnimation(kickInPoint[i], aboutMeLiArray[i])
-        }
+        // for (let i = 0; i < kickInPoint.length; i++) {
+        //     slideInAnimation(kickInPoint[i], aboutMeLiArray[i])
+        // }
     // END: list slide in animation
     
+
     // Languages & tools slide in animation
         const content1Div = document.querySelector('.content1');
         const content2Div = document.querySelector('.content2');
